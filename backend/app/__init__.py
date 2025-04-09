1b6ba954f32e6025ba3d7b53d3c8d51ecfed2025
KI-116 <121534512+KI-116@users.noreply.github.com>
2025-04-09 19:30:28 +0800

    Create __init__.py

---
+++ backend/app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_socketio import SocketIO
+from app.config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+socketio = SocketIO(app)
+
+from app.routes import http_routes, websocket_routes
